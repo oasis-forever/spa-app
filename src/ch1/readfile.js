07332e4d55540627224edab306e9bf3e1ab717ab
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2021-08-16 00:15:51 +0900

    Update src/ch1/readfile.js

--- src/ch1/readfile.js
+++ src/ch1/readfile.js
@@ -4,9 +4,7 @@ const fs = require('fs');
 const data = fs.readFileSync('./text/sample.txt', 'utf-8');
 console.log(data);
 
-// Process after asynchronically reading a file
-const readHandler = (err, data) => {
-  console.log(data);
-};
 // Async Reading
-fs.readFile('./text/sample.txt', 'utf-8', readHandler);
+fs.readFile('./text/sample.txt', 'utf-8', (err, data) => {
+  console.log(data);
+});
